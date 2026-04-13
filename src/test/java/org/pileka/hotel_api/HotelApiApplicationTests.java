5a6fdcbf3427842d34c9fc1238f06231e2505bd3
Pavel Pileka <pavelpileka@gmail.com>
2026-04-13 12:28:38 +0300

    Initial commit

---
+++ src/test/java/org/pileka/hotel_api/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pileka.hotel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
