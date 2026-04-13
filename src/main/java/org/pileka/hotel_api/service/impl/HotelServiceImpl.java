052b1b29e6c0dec036bf0f0ba56c45219714c46b
Pavel Pileka <pavelpileka@gmail.com>
2026-04-13 20:02:21 +0300

    feat: add HotelServiceImpl class

---
+++ src/main/java/org/pileka/hotel_api/service/impl/HotelServiceImpl.java
@@ -0,0 +1,69 @@
+package org.pileka.hotel_api.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.pileka.hotel_api.domain.Hotel;
+import org.pileka.hotel_api.dto.*;
+import org.pileka.hotel_api.exception.EntityDoesntExistException;
+import org.pileka.hotel_api.mapper.HotelMapper;
+import org.pileka.hotel_api.repository.HotelRepository;
+import org.pileka.hotel_api.service.HotelService;
+import org.pileka.hotel_api.specification.HotelSpecificationUtil;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class HotelServiceImpl implements HotelService {
+
+    private final HotelRepository repository;
+
+    private final HotelMapper mapper;
+
+    @Override
+    public ShortResponseHotelDTO create(CreateHotelDTO createDto) {
+        return mapper.toShortDto(
+                repository.save(
+                        mapper.toModel(createDto)
+                )
+        );
+    }
+
+    @Override
+    public ResponseHotelDTO get(long id) {
+        return mapper.toDto(
+                repository.findById(id)
+                        .orElseThrow(() -> new EntityDoesntExistException("Hotel with id " + id + "doesn't exist"))
+        );
+    }
+
+    @Override
+    public List<ShortResponseHotelDTO> get() {
+        return repository.findAll().stream().map(mapper::toShortDto).toList();
+    }
+
+    @Override
+    public List<ShortResponseHotelDTO> get(SearchHotelDTO searchDto) {
+        return repository.findAll(
+                HotelSpecificationUtil.fromSearchDTO(searchDto)
+        ).stream().map(mapper::toShortDto).toList();
+    }
+
+    @Override
+    public List<HistogramDTO> getHistogram(String fieldName) {
+        return repository.getHistogram(fieldName);
+    }
+
+    @Override
+    public void addAmenities(long id, List<String> amenities) {
+        Hotel hotel = repository.findById(id)
+                .orElseThrow(() -> new EntityDoesntExistException("Hotel with id " + id + "doesn't exist"));
+
+        hotel.getAmenities().addAll(amenities);
+
+        repository.save(hotel);
+    }
+}
