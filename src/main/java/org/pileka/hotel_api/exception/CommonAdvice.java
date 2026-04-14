0897a9d51a4c8c660cae90423584ce9b207c9c0e
Pavel Pileka <pavelpileka@gmail.com>
2026-04-14 16:50:34 +0300

    feat: add CommonAdvice class

---
+++ src/main/java/org/pileka/hotel_api/exception/CommonAdvice.java
@@ -0,0 +1,51 @@
+package org.pileka.hotel_api.exception;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CommonAdvice {
+
+    @ExceptionHandler(EntityDoesntExistException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleEntityDoesntExist(EntityDoesntExistException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        Map<String, String> errorInfo = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+
+            errorInfo.put(fieldName, message);
+        });
+
+        return ResponseEntity.badRequest().body(errorInfo.toString());
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleDataAccess(DataAccessException e) {
+        return ErrorResponse.create(e, HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong while accessing the database:" + e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(code= HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleGenericInternalError(RuntimeException e) {
+        return ErrorResponse.create(e, HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error happened: " + e.getMessage());
+    }
+}
