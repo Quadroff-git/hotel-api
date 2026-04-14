6430b1f074c09f2f64b27d31f6dd1d9c0b4ebae7
Pavel Pileka <pavelpileka@gmail.com>
2026-04-14 12:56:55 +0300

    feat: add HotelController interface

---
+++ src/main/java/org/pileka/hotel_api/controller/HotelController.java
@@ -0,0 +1,32 @@
+package org.pileka.hotel_api.controller;
+
+import jakarta.validation.Valid;
+import org.pileka.hotel_api.dto.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public interface HotelController {
+
+    @PostMapping(path = "/hotels")
+    ShortResponseHotelDTO create(@Valid @RequestBody CreateHotelDTO createDto);
+
+    @GetMapping(path = "/hotels/{id}")
+    ResponseHotelDTO get(@PathVariable long id);
+
+    @GetMapping(path = "/hotels")
+    List<ShortResponseHotelDTO> get();
+
+    @GetMapping(path = "/search")
+    List<ShortResponseHotelDTO> get(@Valid SearchHotelDTO searchDto);
+
+    @GetMapping(path = "/histogram/{param}")
+    List<HistogramDTO> getHistogram(@PathVariable String param);
+
+    @PostMapping(path = "/hotels/{id}/amenities")
+    void addAmenities(@RequestBody List<String> amenities);
+
+}
